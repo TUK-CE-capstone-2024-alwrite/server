eaf48061b5d30225cc5014ca0626cc19ea1385fe
ohamin26 <ohamin26@gmail.com>
2024-02-17 19:41:00 +0900

    프로젝트 초기 설정

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from .routes import routes_list
+
+def create_app():
+  app = Flask(__name__)
+  
+  routes_list(app)
+  
+  @app.route('/')
+  def test():
+    return 'test'
+  
+  return app
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
---
+++ src/routes.py
@@ -0,0 +1,5 @@
+#controller 등록
+def routes_list(app):
+  return app
+
+
