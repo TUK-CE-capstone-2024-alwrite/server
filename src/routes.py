6eb90ce9599dc11ae8752c7916b2144d9b7cb58f
ohamin26 <ohamin26@gmail.com>
2024-02-20 18:46:30 +0900

    db 파일 변경

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
-#controller 등록
-def routes_list(app):
-  return app
-
-
+# routes.py 또는 routes_list 함수
+def routes_list(app, database):
+    @app.route('/some_route')
+    def some_route():
+        return database.db.child('some_path').get().val()
