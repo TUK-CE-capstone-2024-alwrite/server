2372c0c3a718efcda92e231133243fbbb1946b87
ohamin26 <ohamin26@gmail.com>
2024-02-20 19:53:21 +0900

    ocr 코드 추가

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from .routes import routes_list
-from .database.database import Database
+from flask import Flask
+from EasyOCR.run import detect as easyocr
 
 def create_app():
     app = Flask(__name__)
-    database = Database()  # Database 클래스의 인스턴스 생성
-
-    routes_list(app, database)  # routes_list 함수에 데이터베이스 인스턴스를 전달
 
     @app.route('/')
     def test():
-        return database.db.child('test').get().val()
+        result = easyocr()
+        print(result)
+        return result
 
     return app
 
