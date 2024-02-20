28dde4b70a0282e1a6b088aa28e47a6ff075282b
ohamin26 <ohamin26@gmail.com>
2024-02-20 18:46:30 +0900

    fix : db 파일 경로 변경

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from .routes import routes_list
-from .database import database as db
+from .database.database import Database
 
 def create_app():
-  app = Flask(__name__)
-  
-  routes_list(app)
-  
-  @app.route('/')
-  def test():
-    return 'hello world!'
-  
-  return app
-  
+    app = Flask(__name__)
+    database = Database()  # Database 클래스의 인스턴스 생성
+
+    routes_list(app, database)  # routes_list 함수에 데이터베이스 인스턴스를 전달
+
+    @app.route('/')
+    def test():
+        return database.db.child('test').get().val()
+
+    return app
+
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', port='5000')
