5897c0ac7544141b8eb56d8b0f90e39c00533333
ohamin26 <ohamin26@gmail.com>
2024-02-21 20:58:18 +0900

    feat : 파일 수신 코드 추가

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from .routes import routes_list
-from flask import Flask
-from EasyOCR.run import detect as easyocr
 
 def create_app():
     app = Flask(__name__)
 
+    routes_list(app)
+
     @app.route('/')
     def test():
-        result = easyocr()
-        print(result)
-        return result
+        return 'test'
 
     return app
 
---
+++ src/controllers/detect_controllers.py
@@ -0,0 +1,37 @@
+import os
+from flask import Blueprint, request
+from flask import jsonify
+from werkzeug.utils import secure_filename
+from EasyOCR.run import detect as easyocr
+from datetime import datetime
+
+import json
+import src.services.detect_service as detect_service
+
+detect_bp = Blueprint(name='detect',import_name=__name__, url_prefix='/detect/')
+
+@detect_bp.route('/', methods=['POST'])
+def detect() -> str:
+    # 파일 수신
+    f = request.files['test']
+    
+    # 저장 폴더 비우기
+    folder = 'EasyOCR/demo_images/'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    
+    # 파일 저장
+    f.save(folder + secure_filename(f.filename))
+    
+    # 수신 받은 파일 텍스트 변환
+    result = easyocr()
+    result = detect_service.detect_route(result)
+    json_result = json.dumps(result, ensure_ascii=False)
+    return json_result
