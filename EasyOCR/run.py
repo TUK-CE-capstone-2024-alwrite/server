0d4b890a3d443d0abc30d24c9fac8cc4ff423f42
ohamin26 <ohamin26@gmail.com>
2024-03-11 18:32:06 +0900

    gitignore / run.py, detect_controller 변수명 수정

--- EasyOCR/run.py
+++ EasyOCR/run.py
@@ -3,7 +3,7 @@ import os
 # GPU 설정
 #os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
-
+#변환할 이미지 가져오기
 def get_files(path):
     file_list = []
 
@@ -16,11 +16,12 @@ def get_files(path):
 
     return file_list, len(file_list)
 
-
-def detect(): # # Using default model
+#이미지 텍스트 변환
+def detect_easyocr(): # # Using default model
     # reader = Reader(['ko'], gpu=True)
 
     # Using custom model
+    # 이미지에서 텍스트를 추출하기 위해 reader 객체 생성
     reader = Reader(['ko','en'], gpu=False,
                     model_storage_directory='EasyOCR/model',
                     user_network_directory='EasyOCR/user_network_dir',
@@ -29,16 +30,16 @@ def detect(): # # Using default model
     files, count = get_files('EasyOCR/demo_images')
     result_set = []
 
+    #reader에서 텍스트 / bbox / confidence 추출
     for idx, file in enumerate(files):
         filename = os.path.basename(file)
 
         result = reader.readtext(file)
-    
         for (bbox, string, confidence) in result:
             
             bbox = [[int(x) for x in point] for point in bbox]
 
-            print("filename: '%s', confidence: %.4f, string: '%s'" % (filename, confidence, string))
+            print("filename: '%s', confidence: %.4f, string: '%s', bbox: %s" % (filename, confidence, string, bbox))
             result_set.append({
                 'filename': filename,
                 'confidence': float(confidence),  # Convert confidence to float
--- src/controllers/detect_controllers.py
+++ src/controllers/detect_controllers.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, request
 from flask import jsonify
 from werkzeug.utils import secure_filename
-from EasyOCR.run import detect as easyocr
+from EasyOCR.run import detect_easyocr as easyocr
 import json
 import src.services.detect_service as detect_service
 
@@ -10,10 +10,16 @@ detect_bp = Blueprint(name='detect',import_name=__name__, url_prefix='/api/detec
 
 @detect_bp.route('/', methods=['POST'])
 def detect() -> str:
+    
     # 파일 수신
-    f = request.files['file']
+    # 실패시 애러 메시지 반환
+    try:
+        file = request.files['file']
+    except:
+        return jsonify({'error': '파일 수신 실패'})
     
     # 저장 폴더 비우기
+    # 실패시 애러 메시지 반환
     folder = 'EasyOCR/demo_images/'
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
@@ -24,13 +30,13 @@ def detect() -> str:
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
+            return jsonify({'error': 'Failed to delete %s. Reason: %s' % (file_path, e)})
     
     # 파일 저장
-    f.save(folder + secure_filename(f.filename))
+    file.save(folder + secure_filename(file.filename))
     
     # 수신 받은 파일 텍스트 변환
     result = easyocr()
     result = detect_service.detect_route(result)
-    # json_result = json.dumps(result, ensure_ascii=False)
-    # return json_result
+    
     return jsonify(result)
