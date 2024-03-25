edd33248cecfe102c652707dc0603f4851a4ffa8
ohamin26 <ohamin26@gmail.com>
2024-03-25 12:38:47 +0900

    db 세팅 변경

--- src/database/database.py
+++ src/database/database.py
@@ -3,7 +3,7 @@ import json
 
 class Database:
     def __init__(self):
-        with open('authentication/firebase_auth.json') as f:
+        with open('secret_info/authentication/firebase_auth.json') as f:
             config = json.load(f)
 
         firebase = pyrebase.initialize_app(config)
